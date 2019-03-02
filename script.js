c5f4968416a3a108d86ed55f83665682b8e227d8
akashyadav <akashyadav256526@gmail.com>
2019-03-02 17:16:03 +0530

    ffinal version

--- script.js
+++ script.js
@@ -3,15 +3,14 @@
 
 // get the selection form player
 function playerSelection(){
-	let x=prompt("Enter your choice from  ROCK PAPER SCISSORS");
-	let y;
+	const x=prompt("Round "+y+"\nEnter your choice from  ROCK PAPER SCISSORS");
 	if (x.charAt(0).toLowerCase()==="r") return 1;
 	else if (x.charAt(0).toLowerCase()==="p") return 2;
 	else return 3;
 }
 // get selection for computer
 function computerSelection(){
-	let  x=Math.random();
+	const  x=Math.random();
 	let y;
 	if (x<=0.333333) y=1;
 	else if (x<0.666666) y=2;
@@ -20,12 +19,61 @@ function computerSelection(){
 }
 //start a round of game
 function playRound() {
-	let player=playerSelection();
-	let computer=computerSelection();
-	if (player==computer) prompt("Tie!");
-	(player==1 & computer==2) ? prompt("Computer wins!") : prompt("Player wins!");
-	(player==2 & computer==3) ? prompt("Computer wins!") : prompt("Player wins!");
-	(player==3 & computer==1) ? prompt("Computer wins!") : prompt("Player wins!");
+	const player=playerSelection();
+	const computer=computerSelection();
+	if (player==computer){ 
+		alert("Tie!");
+		return;
+	}
+	if (player==1){
+		if (player==1 && computer==2) {
+			alert("You Lose! Paper beats Rock");
+			computerScore++;
+	} 
+		else {
+			alert("You Win! Rock beats Scissors");
+			playerScore++;
+		}
+
+	}
+	if (player==2){
+		if (player==2 && computer==3) {
+			alert("You Lose! Scissors beats Paper");
+			computerScore++;
+		} else {
+			alert("You Win! Paper beats Rock");
+			playerScore++;
+		}
+	}
+	if (player==3){
+		if (player==3 && computer==1) {
+			alert("You Lose! Rock beats Scissors");
+			computerScore++;
+		} else {
+			alert("You Win! Scissors beats Paper");
+			playerScore++;
+		}
+	}
 }
 //start
-playRound();
+function game(){
+	 playerScore=0;
+	 computerScore=0;
+	for (y=1;y<=5;y++){
+		playRound(playerScore,computerScore);
+		alert(`The Score is: player ${playerScore} computer ${computerScore}`);
+	}
+	finalScore(playerScore,computerScore);
+}
+game();
+function finalScore(a,b){
+	if (a>b) {
+		alert(`Whhoa! You Won by ${a-b} points`);
+	}
+	 else if (a<b){
+	 	alert(`Shit! You Lose by ${b-a} points`);
+	}
+	else{
+		alert("Fuck! It was a Tie, atleast you didn't lose");
+	}
+}
